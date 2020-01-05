13551f20751457c3cf2d2ebe29c1dae88b9af50b
DmrLutfullin <dmr.lutfullin@gmail.com>
2020-01-05 18:58:14 +0500

    Initial Commit

---
+++ FunctionTests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FunctionTests
+//
+//  Created by Дамир Лутфуллин on 05.01.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
